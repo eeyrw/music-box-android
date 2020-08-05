3708c673f7bff00bcb1acaea4a8611a838c4d7dd
yuan <ayrw@163.com>
2020-08-05 21:32:16 +0800

    M

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,5 @@
 #include <jni.h>
 #include <string>
-#include <oboe/Oboe.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_music_1box_MainActivity_stringFromJNI(
@@ -9,3 +8,86 @@ Java_com_example_music_1box_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+
+extern "C" JNIEXPORT jboolean JNICALL
+Java_com_example_music_1box_MainActivity_runOboeSinePlayer(
+        JNIEnv* env,
+        jobject /* this */) {
+
+    return true;
+}
+
+#include <jni.h>
+#include <string>
+#include <vector>
+
+#include "MegaDroneEngine.h"
+
+
+std::vector<int> convertJavaArrayToVector(JNIEnv *env, jintArray intArray) {
+    std::vector<int> v;
+    jsize length = env->GetArrayLength(intArray);
+    if (length > 0) {
+        jint *elements = env->GetIntArrayElements(intArray, nullptr);
+        v.insert(v.end(), &elements[0], &elements[length]);
+        // Unpin the memory for the array, or free the copy.
+        env->ReleaseIntArrayElements(intArray, elements, 0);
+    }
+    return v;
+}
+
+extern "C" {
+/**
+ * Start the audio engine
+ *
+ * @param env
+ * @param instance
+ * @param jCpuIds - CPU core IDs which the audio process should affine to
+ * @return a pointer to the audio engine. This should be passed to other methods
+ */
+JNIEXPORT jlong JNICALL
+Java_com_example_music_1box_MainActivity_startEngine(JNIEnv *env, jobject /*unused*/,
+                                                                jintArray jCpuIds) {
+    std::vector<int> cpuIds = convertJavaArrayToVector(env, jCpuIds);
+    LOGD("cpu ids size: %d", static_cast<int>(cpuIds.size()));
+    MegaDroneEngine  *engine = new MegaDroneEngine(std::move(cpuIds));
+    LOGD("Engine Started");
+    return reinterpret_cast<jlong>(engine);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_music_1box_MainActivity_stopEngine(JNIEnv *env, jobject instance,
+                                                               jlong jEngineHandle) {
+    auto engine = reinterpret_cast<MegaDroneEngine*>(jEngineHandle);
+    if (engine) {
+        delete engine;
+    } else {
+        LOGD("Engine invalid, call startEngine() to create");
+    }
+}
+
+
+JNIEXPORT void JNICALL
+Java_com_example_music_1box_MainActivity_tap(JNIEnv *env, jobject instance,
+                                                        jlong jEngineHandle, jboolean isDown) {
+
+    auto *engine = reinterpret_cast<MegaDroneEngine*>(jEngineHandle);
+    if (engine) {
+        engine->tap(isDown);
+    } else {
+        LOGE("Engine handle is invalid, call createEngine() to create a new one");
+    }
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_music_1box_MainActivity_native_1setDefaultStreamValues(JNIEnv *env,
+                                                                                   jclass type,
+                                                                                   jint sampleRate,
+                                                                                   jint framesPerBurst) {
+    oboe::DefaultStreamValues::SampleRate = (int32_t) sampleRate;
+    oboe::DefaultStreamValues::FramesPerBurst = (int32_t) framesPerBurst;
+}
+
+} // extern "C"
+
--- app/src/main/java/com/example/music_box/MainActivity.java
+++ app/src/main/java/com/example/music_box/MainActivity.java
@@ -4,8 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = com.example.music_box.MainActivity.class.toString();
+    private static long mEngineHandle = 0;
+
+    private native long startEngine(int[] cpuIds);
+    private native void stopEngine(long engineHandle);
+    private native void tap(long engineHandle, boolean isDown);
+
+    private static native void native_setDefaultStreamValues(int sampleRate, int framesPerBurst);
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -20,11 +37,67 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        boolean b=runOboeSinePlayer();
     }
 
+    @Override
+    protected void onResume(){
+        mEngineHandle = startEngine(getExclusiveCores());
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause(){
+        stopEngine(mEngineHandle);
+        super.onPause();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            tap(mEngineHandle, true);
+        } else if (event.getAction() == MotionEvent.ACTION_UP){
+            tap(mEngineHandle, false);
+        }
+        return super.onTouchEvent(event);
+    }
+
+    // Obtain CPU cores which are reserved for the foreground app. The audio thread can be
+    // bound to these cores to avoids the risk of it being migrated to slower or more contended
+    // core(s).
+    private int[] getExclusiveCores(){
+        int[] exclusiveCores = {};
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.w(TAG, "getExclusiveCores() not supported. Only available on API " +
+                    Build.VERSION_CODES.N + "+");
+        } else {
+            try {
+                exclusiveCores = android.os.Process.getExclusiveCores();
+            } catch (RuntimeException e){
+                Log.w(TAG, "getExclusiveCores() is not supported on this device.");
+            }
+        }
+        return exclusiveCores;
+    }
+
+    static void setDefaultStreamValues(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+            AudioManager myAudioMgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            String sampleRateStr = myAudioMgr.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            int defaultSampleRate = Integer.parseInt(sampleRateStr);
+            String framesPerBurstStr = myAudioMgr.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+            int defaultFramesPerBurst = Integer.parseInt(framesPerBurstStr);
+
+            native_setDefaultStreamValues(defaultSampleRate, defaultFramesPerBurst);
+        }
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native boolean runOboeSinePlayer();
 }
