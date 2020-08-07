b7765c8018cc96307979c015fac22747af655bef
eeyrw <ayrw@163.com>
2020-08-07 09:59:44 +0800

    M

--- app/src/main/cpp/WaveTableSynthesizerSource.h
+++ app/src/main/cpp/WaveTableSynthesizerSource.h
@@ -3,9 +3,6 @@
 
 #include <array>
 #include <TappableAudioSource.h>
-
-#include <Oscillator.h>
-#include <Mixer.h>
 #include <MonoToStereo.h>
 
 #include "WaveTableSynthesizer.h"
@@ -15,21 +12,39 @@ class WaveTableSynthesizerSource : public TappableAudioSource
 public:
     WaveTableSynthesizerSource(int32_t sampleRate, int32_t channelCount) : TappableAudioSource(sampleRate, channelCount)
     {
-
-        if (mChannelCount == oboe::ChannelCount::Stereo)
-        {
-            mOutputStage = &mConverter;
-        }
+        PlayerInit(&player);
     }
 
-    void tap(bool isOn) override{
-
+    void tap(bool isOn) override
+    {
+        if (isOn)
+            PlayerPlay(&player);
+        else
+            PlayerInit(&player);
     };
 
     // From IRenderableAudio
     void renderAudio(float *audioData, int32_t numFrames) override
     {
-        mOutputStage->renderAudio(audioData, numFrames);
+        for (int i = 0; i < numFrames; ++i)
+        {
+            Player32kProc(&player);
+            audioData[i] = (int16_t)(player.mainSynthesizer.mixOut >> 8);
+            PlayerProcess(&player);
+        }
+
+        constexpr int kChannelCountStereo = 2;
+        // We assume that audioData has sufficient frames to hold the stereo output, so copy each
+        // frame in the input to the output twice, working our way backwards through the input array
+        // e.g. 123 => 112233
+        if (mChannelCount == oboe::ChannelCount::Stereo)
+        {
+            for (int i = numFrames - 1; i >= 0; --i)
+            {
+                audioData[i * kChannelCountStereo] = audioData[i];
+                audioData[i * kChannelCountStereo + 1] = audioData[i];
+            }
+        }
     };
 
     virtual ~WaveTableSynthesizerSource()
@@ -38,9 +53,8 @@ public:
 
 private:
     // Rendering objects
-    WaveTableSynthesizer synthesizer;
-    MonoToStereo mConverter = MonoToStereo(&synthesizer);
-    IRenderableAudio *mOutputStage; // This will point to either the mixer or converter, so it needs to be raw
+    int32_t mSampleRate = kDefaultSampleRate;
+    Player player;
 };
 
 #endif //MEGADRONE_SYNTH_H
